9c1ee7afa65fd2a3c0217f3c8ba04b3e01d02f83
PLLima <74931964+PLLima@users.noreply.github.com>
2025-07-01 20:17:40 -0300

    fix: improve minifying filter

--- src/main.cpp
+++ src/main.cpp
@@ -341,7 +341,7 @@ int main(int argc, char* argv[])
     // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 
     // Aprimoramos o escalamento de texturas objetos
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     // Iniciamos o controle da movimentação de câmera livre (por método de Euler)
